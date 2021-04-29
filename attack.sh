4d9be013baca1093cc8e03c8d8bd84a78affa715
Rishab Bamrara <54958658+Rishab057@users.noreply.github.com>
2021-04-29 17:43:59 -0400

    Changed n_runs to 10

--- attack.sh
+++ attack.sh
@@ -1,3 +1,3 @@
 python2 scripts/Poisoning_attacks/attack_DBGD_base_lr.py --data_sets local_MQ2007 --attacker_click_model frequency_attack\
        --click_models exper1 --log_folder ./log --output_folder ./output --average_folder ./average \
-       --n_impr 10000 --n_runs 5 --n_proc 10 --n_results 10 --start 0 --end 1 --which 1 --mf 5 --sd_const 2.0 --num_attacker_relevant 5
+       --n_impr 10000 --n_runs 10 --n_proc 10 --n_results 10 --start 0 --end 1 --which 1 --mf 5 --sd_const 2.0 --num_attacker_relevant 5
